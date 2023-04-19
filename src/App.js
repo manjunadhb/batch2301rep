eb92dd59732f1555646fab451ce46ffca18c74ee
ShaikHabeeb66 <123514014+ShaikHabeeb66@users.noreply.github.com>
2023-04-20 01:15:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,17 @@ function App() {
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Badge_of_the_Indian_Air_Force.svg/1857px-Badge_of_the_Indian_Air_Force.svg.png"></img>
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Indian_Navy_Insignia.svg/1200px-Indian_Navy_Insignia.svg.png"></img>
     </div>
+     <h1>SASS Practice</h1>
+      <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
     </div>
   );
 }
